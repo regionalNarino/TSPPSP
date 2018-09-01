7ebd631d605a12113c7602e0b35aa005e3843259
WORLDSKILLS <elhianp28@gmail.com>
2018-09-01 09:02:32 -0500

    segundo

--- app/src/main/java/com/example/worldskills/tsppsp/MainActivity.java
+++ app/src/main/java/com/example/worldskills/tsppsp/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.worldskills.tsppsp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listProjects;
+    EditText nameProject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listProjects=findViewById(R.id.listProjects);
+        nameProject=findViewById(R.id.txtNameProject);
+
+        cargarList();
+
+    }
+
+    private void cargarList() {
+
+    }
+
+    public void onClick(View view) {
+        adicionar();
+    }
+
+    private void adicionar() {
     }
 }
---
+++ app/src/main/java/com/example/worldskills/tsppsp/Other/Conexion.java
@@ -0,0 +1,23 @@
+package com.example.worldskills.tsppsp.Other;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Conexion extends SQLiteOpenHelper {
+    public Conexion(Context context) {
+        super(context, "tsp", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table proyecto(id integer primary key autoincrement, nombre text, tiempo text)");
+        db.execSQL("create table timelog(id integer primary key autoincrement, phase text, start text, interruption text, stop text, delta text, comments text, idproject text)");
+        db.execSQL("create table defectlog(id integer primary key autoincrement, date text, type text, phaseinjected text, phaseremoved text, fixtime text, defectdescription text, idproject text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
